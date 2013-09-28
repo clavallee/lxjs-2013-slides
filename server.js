b744c6ed6b2c0ccb0168ec86e6e2111071e797b0
James Halliday <mail@substack.net>
2013-09-28 17:04:00 +0100

    slides yay

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var fs = require('fs');
+var marked = require('marked');
 var shoe = require('shoe');
 var shux = require('shux')();
 var muxDemux = require('mux-demux');
@@ -7,6 +9,20 @@ var clear = new Buffer([ 0x1b, 0x5b, 0x48, 0x1b, 0x5b, 0x32, 0x4a ]);
 var ecstatic = require('ecstatic')(__dirname + '/static');
 
 var server = http.createServer(function (req, res) {
+    if (req.url === '/slides') {
+        var ondata = function (err, src) {
+            if (err) return res.end(err + '\n');
+            res.setHeader('content-type', 'text/html');
+            res.end(marked(src))
+        };
+        fs.readFile(__dirname + '/readme.markdown', 'utf8', ondata);
+        return;
+    }
+    
+    if (/^\/static\//.test(req.url)) {
+        req.url = req.url.replace(/^\/static/, '');
+    }
+console.log(req.url);
     ecstatic(req, res);
 });
 server.listen(8000, '127.0.0.1');
